5e4f1b79667d7edda8a04e51f0fe6c8c7f869afb
Balghari <najmbalghari13@gmail.com>
2025-12-23 22:06:59 +0500

    accordion

---
+++ 25-react-practice-project01/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+import "./App.css";
+import Accordion from "./components/Accordion/Accordion";
+
+function App() {
+  return <>
+  <Accordion/>
+  </>;
+}
+
+export default App;
