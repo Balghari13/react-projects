f7bae9d27d425b7231a0b8dc92a4ce2c5afb9163
Balghari <najmbalghari13@gmail.com>
2026-01-06 15:59:08 +0500

    fix height

--- 25-react-practice-project01/src/components/11.model/CustomModelPopup.jsx
+++ 25-react-practice-project01/src/components/11.model/CustomModelPopup.jsx
@@ -5,7 +5,7 @@ const CustomModelPopup = ({body,closeModel}) => {
     <div className='bg-amber-300 p-4'>
       <button className='border-2 bg-amber-950 text-white p-2' onClick={closeModel}>X</button>
       <header className='bg-red-500'>this is header</header>
-      <section className='bg-green-300 min-h-[100px]'>{body || 'this is section of body'}</section>
+      <section className='bg-green-300 min-h-25'>{body || 'this is section of body'}</section>
       <footer className='bg-orange-400'>this is footer</footer>
     </div>
   )
